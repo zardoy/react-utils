be62dfc4cdb5e153b54b64e573e21a2b40ca7e72
Vitaly Turovsky <vital2580@icloud.com>
2024-05-01 19:55:16 +0300

    feat: useChainId

---
+++ src/useChainId.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react'
+import { useChainId } from 'wagmi'
+import { watchNetwork } from '@wagmi/core'
+
+let watched = false
+export default (supportedChains: {id, name, etherscan?}[]) => {
+    const _chainId = useChainId()
+    const [chainId, setChainId] = useState<number | null>(_chainId)
+
+    useEffect(() => {
+        if (watched) return
+        watched = true
+        watchNetwork(({ chain }) => {
+            if (chain?.id) {
+                setChainId(chain.id)
+            }
+        })
+    }, [])
+
+    const currentChain = supportedChains.find(({ id }) => id === chainId)
+
+    return {
+        chainId,
+        supportedName: supportedChains[0]?.name || '',
+        isUnsupported: !currentChain,
+        etherscan: supportedChains[0]?.etherscan || '',
+    }
+}
