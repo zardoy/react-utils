ebdfd869d62ec909c510f2cb139d750c62b31343
Vitaly Turovsky <vital2580@icloud.com>
2024-05-02 11:36:00 +0300

    up ts

--- src/useChainId.tsx
+++ src/useChainId.tsx
@@ -2,14 +2,11 @@ import { useEffect, useState } from 'react'
 import { useChainId } from 'wagmi'
 import { watchNetwork } from '@wagmi/core'
 
-let watched = false
 export default (supportedChains: {id, name, etherscan?}[]) => {
     const _chainId = useChainId()
     const [chainId, setChainId] = useState<number | null>(_chainId)
 
     useEffect(() => {
-        if (watched) return
-        watched = true
         watchNetwork(({ chain }) => {
             if (chain?.id) {
                 setChainId(chain.id)
